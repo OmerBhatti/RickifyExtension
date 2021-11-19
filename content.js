3fbda016ae74984666961f2106c43e9674645f61
Omerbhatti <Omerbhatti34@gmail.com>
2021-11-19 21:05:28 +0500

    Fixed Image Not loading bug

--- content.js
+++ content.js
@@ -8,13 +8,14 @@ function RickifyImage(){
 function RickifyVideo(){
   document.querySelectorAll("video")
   .forEach(video => {
-    video.parentElement.innerHTML = `<div style="width:100%;height:0px;position:relative;padding-bottom:56.250%;"><iframe src="https://streamable.com/e/0w4gz5?autoplay=1" frameborder="0" width="100%" height="100%" allowfullscreen allow="autoplay" style="width:100%;height:100%;position:absolute;left:0px;top:0px;overflow:hidden;"></iframe></div>`;
+    video.parentElement.innerHTML = `<div style="width:100%;height:0px;position:relative;padding-bottom:56.250%;"><iframe src="https://streamable.com/e/0w4gz5?autoplay=1" frameborder="0" width="100%" height="100%" allow="autoplay" style="width:100%;height:100%;position:absolute;left:0px;top:0px;overflow:hidden;"></iframe></div>`;
   })
 }
 
 RickifyImage();
-RickifyVideo();
 
 setInterval(() => {
   RickifyImage();
 }, 500);
+
+RickifyVideo();
